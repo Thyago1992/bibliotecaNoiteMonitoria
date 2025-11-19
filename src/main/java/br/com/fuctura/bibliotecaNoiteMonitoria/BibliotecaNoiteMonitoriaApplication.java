2b2bbfc172cdc686e9f628e380b33e9ea664f85a
Thyago <thyagoa727@gmail.com>
2025-11-19 01:12:20 -0300

    Aula 18/11

---
+++ src/main/java/br/com/fuctura/bibliotecaNoiteMonitoria/BibliotecaNoiteMonitoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fuctura.bibliotecaNoiteMonitoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaNoiteMonitoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaNoiteMonitoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fuctura/bibliotecaNoiteMonitoria/BibliotecaNoiteMonitoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fuctura.bibliotecaNoiteMonitoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaNoiteMonitoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
